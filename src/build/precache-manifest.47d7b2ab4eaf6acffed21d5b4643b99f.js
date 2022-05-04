287e6daef814ce513ef8e90d5e90ba9a0e1df517
Dani <danimac@1791.com>
2022-05-04 13:49:24 -0400

    first commit

---
+++ src/build/precache-manifest.47d7b2ab4eaf6acffed21d5b4643b99f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94c44ba9caf113185d59",
+    "url": "/static/css/main.27b68fb0.chunk.css"
+  },
+  {
+    "revision": "94c44ba9caf113185d59",
+    "url": "/static/js/main.ea452f59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9a1bb037419c9f4e5c2",
+    "url": "/static/js/2.473a2292.chunk.js"
+  },
+  {
+    "revision": "d22476f594bce1112a160578955d21ad",
+    "url": "/index.html"
+  }
+];
